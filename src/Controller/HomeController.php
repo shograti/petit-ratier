90f30668b2c2da648d77c791c354941e0b8534a8
shograti <echecsleo@gmail.com>
2022-02-01 16:02:20 +0100

    displayoffers

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\OfferRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,12 +11,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'home')]
-    public function index(OfferRepository $offer_repo): Response
+    public function index(OfferRepository $offer_repo,CategoryRepository $category_repo): Response
     {
         $offers = $offer_repo->findAll();
+        $category = $category_repo->findAll();
+        
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'offers' => $offers,
+            'categories'=> $category
             
         ]);
     }

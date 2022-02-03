651a141209e4ea1699076651f35529bb00b654f1
Peter dew <dewavrinpierre@gmail.com>
2022-02-03 13:04:20 +0100

    Security & single email

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -15,9 +15,11 @@ use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 use Symfony\Component\Validator\Constraints\Date;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class RegisterController extends AbstractController
 {
@@ -31,7 +33,7 @@ class RegisterController extends AbstractController
     }
 
     #[Route('/register', name: 'register')]
-    public function createUser(UserPasswordHasherInterface $passwordHasher, Request $request, EntityManagerInterface $manager, MailerInterface $mailer): Response
+    public function createUser(UserPasswordHasherInterface $passwordHasher, Request $request, EntityManagerInterface $manager, MailerInterface $mailer, UserRepository $userRepository): Response
     {
         $user = new User();
 
@@ -47,54 +49,57 @@ class RegisterController extends AbstractController
             $user->setRatingUser(0);
             $user->setRoleUser("ROLE_USER");
             $manager->persist($user);
-            //* if (security verification)
-            //* TO DO VERIFICATION SINGLE USER IN DB
-            $manager->flush();
-            // dd((string) $user->getIdUser());
-            $signatureComponents = $this->verifyEmailHelper->generateSignature(
-                'registration_confirmation_route',
-                $user->getEmailUser(),
-                (string)$user->getIdUser(),
-                ['id' => $user->getIdUser()]
-            );
-            $email = new TemplatedEmail();
-            $email->from('send@example.com');
-            $email->to($user->getEmailUser());
-            $email->htmlTemplate('register/confirmation_email.html.twig');
-            $email->context(['signedUrl' => $signatureComponents->getSignedUrl()]);
-            $this->mailer->send($email);
+            //* Security Email //
+            if ($userRepository->findOneBy(array('emailUser' => $user->getEmailUser())) == null) {
+                $manager->flush();
+                $signatureComponents = $this->verifyEmailHelper->generateSignature(
+                    'registration_confirmation_route',
+                    $user->getEmailUser(),
+                    (string)$user->getIdUser(),
+                    ['id' => $user->getIdUser()]
+                );
+                $email = new TemplatedEmail();
+                $email->from('send@example.com');
+                $email->to($user->getEmailUser());
+                $email->htmlTemplate('register/confirmation_email.html.twig');
+                $email->context(['signedUrl' => $signatureComponents->getSignedUrl()]);
+                $this->mailer->send($email);
+            } else {
+                $this->addflash("error", "The Email allready exist");
+                throw $this->createNotFoundException('The Email allready exists');
+            }
         }
         return $this->render('register/index.html.twig', [
             'controller_name' => 'RegisterController',
             "form" => $form->createView(),
         ]);
+
     }
 
     #[Route('/auth/verify', name: 'registration_confirmation_route')]
-   public function verifyUserEmail(Request $request, UserRepository $userRepository, EntityManagerInterface $manager): Response
-   {
-       $id = $request->get('id');
-       // Verify the user id exists and is not null
-       if (null === $id) {
-       // User do not exist
+    public function verifyUserEmail(Request $request, UserRepository $userRepository, EntityManagerInterface $manager): Response
+    {
+        $id = $request->get('id');
+        // Verify the user id exists and is not null
+        if (null === $id) {
+            // User do not exist
 
-           return $this->redirectToRoute('home');
-       }
-       $user = $userRepository->find($id);
-       // Ensure the user exists in persistence
-       if (null === $user) {
-           return $this->redirectToRoute('home');
-       }
-       try {
-           $this->verifyEmailHelper->validateEmailConfirmation($request->getUri(), $user->getIdUser(), $user->getEmailUser());
-       }
-       catch (VerifyEmailExceptionInterface $e) {
-           $this->addFlash('verify_email_error', $e->getReason());
-       }
+            return $this->redirectToRoute('home');
+        }
+        $user = $userRepository->find($id);
+        // Ensure the user exists in persistence
+        if (null === $user) {
+            return $this->redirectToRoute('home');
+        }
+        try {
+            $this->verifyEmailHelper->validateEmailConfirmation($request->getUri(), $user->getIdUser(), $user->getEmailUser());
+        } catch (VerifyEmailExceptionInterface $e) {
+            $this->addFlash('verify_email_error', $e->getReason());
+        }
         $user->setRoleUser("ROLE_AUTHORIZED_USER");
-       $this->addFlash('success', 'Your e-mail address has been verified.');
-       $manager->persist($user);
-       $manager->flush();
-       return $this->redirectToRoute('login');
-   }
+        $this->addFlash('success', 'Your e-mail address has been verified.');
+        $manager->persist($user);
+        $manager->flush();
+        return $this->redirectToRoute('login');
+    }
 }
